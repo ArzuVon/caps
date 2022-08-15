57ef05af1c6582a24b32e4c5f0233ec2364b7e56
Von <107226923+ArzuVon@users.noreply.github.com>
2022-08-15 11:55:58 -0400

    vendor logic

--- events/modular/delivery/vendor/vendor.js
+++ events/modular/delivery/vendor/vendor.js
@@ -2,33 +2,33 @@
 
 const events = require('./event-package');
 
-// The brain will emit a "light" event with some payload ... here's how the eyes handle that
-// Note, the eyes module probably cares about a billion other events ...
+// The delivery will emit a "status" event with some payload ... here's how the vendor handle that
+// Note, the vendor module probably cares about a billion other events ...
 // Interesting ... we're the ones that started this whole thing with the setTimeout at the bottom
-events.on('deliver', driver);
+events.on('deliver', vendor);
 events.on('deliver', pickup);
 
 function pickup(payload) {
-  console.log('Package has Customer ID and location to send', payload.brightness, 'status ready');
+  console.log('Package has Customer ID and location to send', payload.status, 'status ready');
 }
 
-function eyelid(payload) {
-  if (payload.brightness >= 75) {
-    console.log('eyes are squinting');
+function vendor(payload) {
+  if (payload.status === true ) {
+    console.log('Package has Customer ID, driver and location to send');
   }
 }
 
-// The eyes see all!
-// Let's simulate walking outside
-// Every 2 seconds, tell the brain how much light we detected
+// The vendor see all!
+// Let's simulate pickup package
+// Every 30 seconds, tell the delivery will know how much packages we detect
 setInterval(() => {
   // Set a random number on a scale of 1-100 for how bright it is
-  let brightness = Math.ceil(Math.random() * 100)
+  let status = Math.ceil(Math.random() * 100)
   console.log('--------------------------------');
-  console.log('Brightness Detected:', brightness);
+  console.log('Package status Detected:', status);
 
   // Tell the brain, we see some light
-  events.emit('light-detected', brightness)
-}, 2000)
+  events.emit('Package status Detected', status)
+}, 30000)
 
-module.exports = { pupil, eyelid }
+module.exports = { pickup, vendor }
